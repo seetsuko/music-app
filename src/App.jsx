2e381cf953fc8cdafdea8f36c4b6e0d13dfb5a35
seetsuko <radgjms@gmail.com>
2023-06-06 14:35:31 +0900

    feat: HeaderとRouterのコンポーネントを作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import React from "react"
+import { Header } from "./Header";
+import { Router } from "./routes/Ruter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+       <Router/>
     </div>
   );
 }
---
+++ src/routes/Ruter.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const Router = () =>{
+  return(
+    <div>ルーティングページ</div>
+  )
+}
